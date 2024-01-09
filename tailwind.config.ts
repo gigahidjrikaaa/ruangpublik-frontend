e9f4d1f6adc503c3f3c7ef89d2631eab010adcbd
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-09 21:34:55 +0700

    feat: define some colors and font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "jakarta": ["Plus Jakarta Sans", "sans-serif"]
       },
+      colors: {
+        blue: {
+          100: "#D6E4FD",
+          200: "#AEC8FC",
+          300: "#85A8F8",
+          400: "#658DF1",
+          500: "#3563E9",
+          600: "#264BC8",
+          700: "#1A37A7",
+          800: "#102587",
+          900: "#0A196F"
+        },
+        secondary: {
+          100: "#E0E9F4",
+          200: "#C3D4E9",
+          300: "#90A3BF",
+          400: "#596780",
+          500: "#1A202C",
+          600: "#131825",
+          700: "#0D121F",
+          800: "#080C19",
+          900: "#040815"
+        }
+      }
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
