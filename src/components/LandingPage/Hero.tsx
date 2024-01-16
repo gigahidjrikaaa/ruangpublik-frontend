2bf078b94b628070f95570ff3e021d10f7118257
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-16 21:21:19 +0700

    feat: make Hero responsive

--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 
 export default function Hero() {
   return(
-    <main className="min-h-screen bg-red-500 bg-[url(/HeroBG.jpg)] bg-cover flex flex-col justify-between items-center pt-[100px] pb-[50px]">
-      <h1 className="text-[72px] font-semibold">Suaramu, suara rakyat Indonesia</h1>
+    <main className="min-h-screen bg-red-500 bg-[url(/HeroBG.jpg)] bg-cover bg-center flex flex-col justify-between items-center pt-[100px] pb-[50px]">
+      <h1 className="px-[5%] md:px-10 text-center text-[45px] md:text-[60px] lg:text-[72px] leading-[105%] font-semibold">Suaramu, suara rakyat Indonesia</h1>
       <Link className="flex flex-col justify-center items-center gap-3" href="#intro">
         <h1 className="text-[20px]">Suarakan Sekarang.</h1>
         <Image src={ScrollDown} alt="Scroll Down" className="rounded-[14px] animate-bounce" />
