f078627423b21476a748b7de0d0390661ee501bc
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-19 06:33:04 +0700

    feat: done Register section

--- src/components/LandingPage/Register.tsx
+++ src/components/LandingPage/Register.tsx
@@ -1,9 +1,15 @@
+import Link from "next/link";
+import PrimaryButton from "../PrimaryButton";
+
 export default function Register() {
   return (
     <section className="min-h-screen flex flex-col justify-start items-center bg-[url(/Register.jpg)]">
       <h1 className="mt-[100px] lg:mt-[120px] px-[5%] md:px-10 text-center text-[28px] md:text-[32px] leading-[105%] font-semibold relative z-[1] text-shadow">
         Bergabunglah dan buat Indonesia menjadi lebih baik.
       </h1>
+      <Link href="/register">
+        <PrimaryButton text="DAFTAR SEKARANG" onClick={() => {}} className="mt-[30px] text-[33px]" />
+      </Link>
     </section>
   );
 }
