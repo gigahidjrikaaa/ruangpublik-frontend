185d09fba343c18a47e90e7fabb19c9c4a948d2b
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-19 06:30:34 +0700

    feat: create SecondaryButton component

--- src/components/SecondaryButton.tsx
+++ src/components/SecondaryButton.tsx
@@ -7,7 +7,7 @@ interface Props {
   iconRight?: React.ReactNode;
 }
 
-export default function PrimaryButton(props: Props) {
+export default function SecondaryButton(props: Props) {
   if (!props.text && !props.iconLeft && !props.iconRight) {
     throw new Error("PrimaryButton must have either (or both) text or icon");
   }
@@ -18,7 +18,7 @@ export default function PrimaryButton(props: Props) {
         if (props.disabled) return;
         props.onClick();
       }}
-      className={`bg-blue-500 flex gap-2 rounded-[4px] text-[13px] font-semibold font-jakarta px-4 py-[10px] transition duration-150 hover:bg-blue-600 active:bg-blue-700 disabled:opacity-80 disabled:cursor-not-allowed ${props.className} `}
+      className={`bg-white text-[#596780] outline outline-1 outline-[#90A3BF] flex gap-2 rounded-[4px] text-[13px] font-semibold font-jakarta px-4 py-[10px] transition duration-150 hover:outline-[#596780] hover:text-[#1A202C] active:bg-blue-100 disabled:outline-blue-100 disabled:opacity-90 disabled:cursor-not-allowed ${props.className} `}
     >
       {props.iconLeft}
       {props.text}
