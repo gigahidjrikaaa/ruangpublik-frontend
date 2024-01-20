8b728b34c9a0072e7280dc20ca0f974916f606a2
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-20 10:34:24 +0700

    feat: adjust navigation routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -119,7 +119,7 @@ export default function Navbar() {
         <ul>
           <MenuLink
             label="Beranda"
-            path="/home"
+            path="/"
             active={activePage}
             icon={
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
+import ConfirmModal from "@/components/ConfirmModal";
 import Hero from "@/components/LandingPage/Hero";
 import Intro from "@/components/LandingPage/Intro";
 import Register from "@/components/LandingPage/Register";
+import { useState } from "react";
 
 export default function Home() {
+  const [isModalOpen, setIsModalOpen] = useState(true);
   return (
     <main>
       <Hero />
       <Intro />
       <Register />
+      {
+        isModalOpen &&
+        <ConfirmModal
+          message="この内容で登録しますか？"
+          additionalMessage="wibu njir"
+          onConfirm={() => alert("登録しました")}
+          onCancel={() => setIsModalOpen(false)}
+        />
+      }
     </main>
   );
 }
