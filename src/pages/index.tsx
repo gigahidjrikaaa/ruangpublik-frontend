f6a3c355acee6e66a7eb16d2fd12ec4b0417d380
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-20 11:48:06 +0700

    feat: install and customize react toastify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Hero from "@/components/LandingPage/Hero";
 import Intro from "@/components/LandingPage/Intro";
 import Register from "@/components/LandingPage/Register";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 export default function Home() {
   const [isModalOpen, setIsModalOpen] = useState(true);
@@ -11,15 +12,32 @@ export default function Home() {
       <Hero />
       <Intro />
       <Register />
-      {
-        isModalOpen &&
+      {isModalOpen && (
         <ConfirmModal
           message="この内容で登録しますか？"
           additionalMessage="wibu njir"
-          onConfirm={() => alert("登録しました")}
+          onConfirm={() => {
+            toast.info("登録しました");
+            toast.warn("登録しました");
+            toast.error("登録しました");
+            toast.success("登録しました");
+            setIsModalOpen(false);
+            toast.loading("登録しました");
+            setTimeout(() => { 
+              const random = Math.random();
+              if (random > 0.5) {
+                toast.success("登録しました");
+              } else {
+                toast.error("登録に失敗しました");
+              }
+            }, 3000);
+            setTimeout(() => {
+              toast.dismiss();
+            }, 5000);
+          }}
           onCancel={() => setIsModalOpen(false)}
         />
-      }
+      )}
     </main>
   );
 }
