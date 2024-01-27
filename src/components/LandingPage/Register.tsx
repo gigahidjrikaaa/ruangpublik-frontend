a2d9c611db8a7ab3d214b12da2922229be48c620
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-01-27 10:39:56 +0700

    feat: aligning with design

--- src/components/LandingPage/Register.tsx
+++ src/components/LandingPage/Register.tsx
@@ -3,7 +3,7 @@ import PrimaryButton from "../PrimaryButton";
 
 export default function Register() {
   return (
-    <section className="relative min-h-screen flex flex-col justify-start items-center bg-[url(/Register.jpg)] text-white bg-cover">
+    <section className="relative min-h-screen flex flex-col justify-center lg:justify-start items-center bg-[url(/Register.jpg)] text-white bg-cover">
       <div className="absolute inset-0 bg-black opacity-50 z-0"></div> 
       <h1 className="mt-[100px] lg:mt-[120px] px-[5%] md:px-10 text-center text-[28px] md:text-[32px] leading-[105%] font-semibold relative z-[1] text-shadow">
         Bergabunglah dan buat Indonesia menjadi lebih baik.
